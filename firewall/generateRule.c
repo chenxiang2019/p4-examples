cb47537a9e0a23c896e12ece23bed20e37240589
Wasdns <wasdnsxchen@gmail.com>
2017-10-13 02:34:17 -0400

    generateRule.c: prefix 24bits lpm

--- firewall/generateRule.c
+++ firewall/generateRule.c
@@ -2,8 +2,8 @@
 
 int main() {
 	for (int i = 2; i <= 100; i++) {
-		printf("table_add firewall forward 10.0.0.%d/32 10.0.0.%d 17 => 3\n", i, i+1);
+		printf("table_add firewall forward 10.0.0.%d/24 10.0.0.%d 17 => 3\n", i, i+1);
 	}
-	printf("table_add firewall forward 10.0.0.1/32 10.0.0.2 17 => 2\n");
+	printf("table_add firewall forward 10.0.0.1/24 10.0.0.2 17 => 2\n");
 	return 0;
 }
