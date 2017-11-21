ccb817c41dc25c68d281dff51144c6654af833b4
XiangChen <wasdnsxchen@gmail.com>
2017-11-21 09:20:49 +0800

    Make moongen tests slighter (#4)
    * Update rule for slight tests
    
    * Delete generateRule.c
    
    * Delete generateRule.sh
    
    * Update rule for slight tests
    
    * Delete generateRule.c
    
    * Delete generateRule.sh
    
    * Update rule for slight tests
    
    * Delete generateRule.c
    
    * Delete generateRule.sh

--- moongen/firewall/generateRule.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-	for (int i = 2; i <= 100; i++) {
-		printf("table_add firewall forward 10.0.0.%d/32 10.0.0.%d 17 => 3\n", i, i+1);
-	}
-	printf("table_add firewall forward 10.0.0.1/32 10.0.0.2 17 => 2\n");
-	return 0;
-}
--- moongen/firewall/generateRule.sh
+++
@@ -1,3 +0,0 @@
-gcc generateRule.c -std=c99
-./a.out > rule/firewall.txt
-rm -rf a.out
